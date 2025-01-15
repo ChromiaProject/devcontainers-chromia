274319eda063b4010becc0e49a94f39cb78a1ce9
Thomas-Chromaway <122302070+Thomas-Chromaway@users.noreply.github.com>
2025-01-15 19:22:35 +0000

    Initial cut at Advanced Chromia with EVM
    This doesn't establish a bridge, but it does introduce an EVM environment.
    
    Also, refactors the environment variables.

---
+++ src/advanced-chromia/evm/scripts/deploy-fixture-and-run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Exit script on error
+set -e
+
+env
+
+# Launch anvil in the background
+anvil --host 0.0.0.0 &
+sleep 2
+
+# Step 1: Compile the contract
+echo "Compiling contract..."
+forge build /app/evm/contracts/
+
+# Step 2: Deploy the contract
+echo "Deploying contract..."
+DEPLOYED_ADDRESS=$(cast send --value 0 --private-key $RAW_PRIVATE_KEY --json --create $(forge inspect $CONTRACT_PATH bytecode) "constructor(uint256)" "42" | sed -e 's/^.*"contractAddress":"\([^"]*\)".*$/\1/' );
+
+echo "Contract deployed at address: '$DEPLOYED_ADDRESS'"
+
+# Step 3: Verify deployment
+echo "Verifying deployment..."
+cast call $DEPLOYED_ADDRESS "balanceOf(address owner) (uint256)" $DEPLOYED_ADDRESS
+
+sleep infinity
--- test/advanced-chromia/test.sh
+++ test/advanced-chromia/test.sh
@@ -4,9 +4,12 @@ source test-utils.sh
 
 # Template specific tests
 check "distro" lsb_release -c
-check "psql" psql -c "\\dt"
+check "psql" sleep 2 && psql -c "\\dt"
 check "chr" chr --version
 check "pmc" pmc --version
+check "ethereum" sleep 2 & curl -X POST -H "Content-Type: application/json" \
+    --data '{"jsonrpc":"2.0","method":"web3_clientVersion","params":[],"id":1}' \
+    http://$ETH_RPC_URL:8545
 
 # Report result
 reportResults
